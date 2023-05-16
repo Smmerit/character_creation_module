d5987d0b8727a0a1cb314a55170339859196117e
Smmerit <130224067+Smmerit@users.noreply.github.com>
2023-05-16 15:36:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-# .../Dev/character_creation_module/main.py
-
-# Импортируем функцию стандартного модуля random.
 from random import randint
 
-
 class Character:
+        # Константа для диапазона очков урона.
+    RANGE_VALUE_ATTACK = (1, 3)
 
     def __init__(self, name):
         self.name = name
-    
+
     def attack(self):
-        # Описываем метод атаки.
-        return (f'{self.name} нанёс противнику урон, равный '
-                f'{5 + randint(5, 10)}')
-    
+        # Вместо диапазона записана переменная класса.
+        # Оператор * распаковывает передаваемый кортеж.
+        value_attack = 5 + randint(*self.RANGE_VALUE_ATTACK)
+        return (f'{self.name} нанёс противнику урон, равный {value_attack}')
+   
     def defence(self):
         ...
 
