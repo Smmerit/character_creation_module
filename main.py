6985da44d13778d99b34e362f1b19f08d9c92be6
Smmerit <130224067+Smmerit@users.noreply.github.com>
2023-05-16 15:33:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 class Character:
-    def __init__(self, name, choice_class):
+    def __init__(self, name):
         self.name = name
-        self.choice_class = choice_class
-        self.attack = attack
-        self.defence = defence
-        self.special = special
-
-
+    
+    def attack(self):
+        ...
+    
+    def defence(self):
+        ...
+    
+    def special(self):
+        ...
 
 class Warrior(Character):
     ...
-class Healer(Character):
-    ...
+
 class Mage(Character):
     ...
-    
+
+class Healer(Character):
+    ...
