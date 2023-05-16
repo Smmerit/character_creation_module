b897b2379f2fa24ed9b7810925de354576f96aea
Smmerit <130224067+Smmerit@users.noreply.github.com>
2023-05-16 15:35:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+# .../Dev/character_creation_module/main.py
+
+# Импортируем функцию стандартного модуля random.
+from random import randint
+
+
 class Character:
+
     def __init__(self, name):
         self.name = name
     
     def attack(self):
-        ...
+        # Описываем метод атаки.
+        return (f'{self.name} нанёс противнику урон, равный '
+                f'{5 + randint(5, 10)}')
     
     def defence(self):
         ...
-    
+
     def special(self):
         ...
 
